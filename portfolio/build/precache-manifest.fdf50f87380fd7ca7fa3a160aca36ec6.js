7e4721b2965755c75639e50f511cd28758b8c3e9
Timothy DiRusso <timgdirusso@gmail.com>
2020-08-08 16:36:39 -0700

    Adding build files for Heroku branch

---
+++ portfolio/build/precache-manifest.fdf50f87380fd7ca7fa3a160aca36ec6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c400e3097ce1d6bb8c29903adbd8f12a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d146bf86c864e18fc392",
+    "url": "/static/css/main.aa8eaa3d.chunk.css"
+  },
+  {
+    "revision": "09fd568072c10786a895",
+    "url": "/static/js/2.88663df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88663df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d146bf86c864e18fc392",
+    "url": "/static/js/main.485cc5b3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "d5c612058666a5cd6b7f8a37a8695491",
+    "url": "/static/media/abbott.d5c61205.png"
+  },
+  {
+    "revision": "24d88d3135cc4c4bab1da88829e534f3",
+    "url": "/static/media/accenture.24d88d31.png"
+  },
+  {
+    "revision": "0c1bfea914c5171ba542329ed70abd08",
+    "url": "/static/media/asu.0c1bfea9.png"
+  },
+  {
+    "revision": "5b47962f06f115180f412d24eff15d7b",
+    "url": "/static/media/bitmoji-profile.5b47962f.png"
+  },
+  {
+    "revision": "62a052398ee850257120c5efbc22d00b",
+    "url": "/static/media/bitmoji-questions.62a05239.png"
+  },
+  {
+    "revision": "ec24a3018fe4e28f470b2454f9f27dbe",
+    "url": "/static/media/bitmoji-studying.ec24a301.png"
+  },
+  {
+    "revision": "5f64de0004accadc1b9ee5387079b1a0",
+    "url": "/static/media/bitmoji-waving.5f64de00.png"
+  },
+  {
+    "revision": "039d6631d7e8724f3ef30784fca6cfea",
+    "url": "/static/media/canon.039d6631.png"
+  },
+  {
+    "revision": "60e2bf8169908d7a009bcea6a62a12b6",
+    "url": "/static/media/citrix.60e2bf81.png"
+  },
+  {
+    "revision": "912532f88012cf7e9c71e56aa77d74eb",
+    "url": "/static/media/dashboard.912532f8.png"
+  },
+  {
+    "revision": "735f63cf70519e9e2a9f716309c14176",
+    "url": "/static/media/deletedSongs.735f63cf.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/framework.580ffd26.png"
+  },
+  {
+    "revision": "2d3049b39e60481ac6f9c7ccbcb2a37e",
+    "url": "/static/media/ge.2d3049b3.png"
+  },
+  {
+    "revision": "eabe35cb793644cb2a15df25400e560a",
+    "url": "/static/media/kbr.eabe35cb.png"
+  },
+  {
+    "revision": "d7eb1156424547336d9926f900f5d75a",
+    "url": "/static/media/languages.d7eb1156.png"
+  },
+  {
+    "revision": "22189085669c5dff4fc44db8f64f0d89",
+    "url": "/static/media/logmein.22189085.png"
+  },
+  {
+    "revision": "424c5f6d9de1550fb47a068daa6e2a66",
+    "url": "/static/media/redrock.424c5f6d.png"
+  },
+  {
+    "revision": "917346330a9b9c8631ea22265e85909f",
+    "url": "/static/media/sunstate.91734633.png"
+  },
+  {
+    "revision": "07b599480352eb5226fc999ef4392ad0",
+    "url": "/static/media/vans.07b59948.png"
+  },
+  {
+    "revision": "d6f8f2108f40bb7e714aa472ab317181",
+    "url": "/static/media/visualizer-artists.d6f8f210.png"
+  },
+  {
+    "revision": "ede080b2ef88f6199befe10f414f00ae",
+    "url": "/static/media/visualizer-tracks.ede080b2.png"
+  }
+]);
